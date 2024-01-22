d3a866c1de3c25a4ae192be7f83b29fa2d946ad2
Berat-Guclu <153362749+Berat-Guclu@users.noreply.github.com>
2024-01-22 21:46:14 +0300

    Update deneme.php

--- deneme.php
+++ deneme.php
@@ -10,7 +10,6 @@ define('ALTLIMIT',1);
 
 
 
-
 if ($mysqli->connect_error) {
     die("Connection failed: " . $mysqli->connect_error);
 }
@@ -19,104 +18,119 @@ if ($mysqli->connect_error) {
 function odenenler($mysqli)
 {
     $sql= "SELECT * FROM tester ";
-    $odenenler_data=array(0=>array());
+    $odenenler_data=array();
     $result = $mysqli->query($sql);
     if ($result->num_rows > 0) {
 
         while ($row = $result->fetch_assoc()) {
 
-            $odenenler_data[$row['rezno']][0] =  $row['rezno'];
-            $odenenler_data[$row['rezno']][1] =  floatval($row['tutar']);
+            $odenenler_data[$row['rezno']]['rezervasyon_no'] =  $row['rezno'];
+            $odenenler_data[$row['rezno']]['tutar'] =  floatval($row['tutar']);
         }
     }
 
-    unset($odenenler_data[0]);
 
     return $odenenler_data;
 }
-
-function getcc($idstring,$conn)
+function getcc($reznoarray,$conn)
 {
     $ccinfo = array();
-    
-    $sql = "SELECT rezervasyon_no,tutar,odeme FROM poslar WHERE rezervasyon_no IN ($idstring) ";
-    $result = $conn->query($sql);
+
+    $reznocount=count($reznoarray);
+    $params=str_repeat("?,",$reznocount);
+    $params=rtrim($params,",");
+    $types=str_repeat("s",$reznocount);
+
+
+    $sql = "SELECT rezervasyon_no,tutar,odeme FROM poslar WHERE rezervasyon_no IN ($params) ";
+    $stmt=$conn->prepare($sql);
+
+
+    $stmt->bind_param($types, ...$reznoarray);
+    $stmt->execute();
+
+    $result=$stmt->get_result();
 
     if ($result->num_rows > 0) {
         while ($row = $result->fetch_assoc()) {
-            @$ccinfo[$row['rezervasyon_no']][0] +=  floatval($row['tutar']);
-            @$ccinfo[$row['rezervasyon_no']][1] .= $row['odeme'];
+            @$ccinfo[$row['rezervasyon_no']]['tutar'] +=  floatval($row['tutar']);
+            @$ccinfo[$row['rezervasyon_no']]['odeme'] .= $row['odeme'];
         }
     }
     return $ccinfo;
 }
+
+
+
 function preparestring($data)
 {
     $string="";
     $idarray = array();
     foreach ($data as $id){
 
-        $string .= "'";
-        $string .= $id[0];
-        $string .= "'";
 
-        $string.=",";
 
-        $idarray[] = $id[0];  ;
+        $idarray[] = $id['rezervasyon_no'];  ;
     }
 
-    $string=rtrim($string, ",");
-    $stringdata = array($string,$idarray);
 
-    return    $stringdata ;
+    return    $idarray;
 }
 
-function gethavale($reznoarray,$conn)
+function gethavale($reznoarray, $conn)
 {
     $havaleinfo = array();
 
-    foreach ($reznoarray as $rezno){
-        $sql = "SELECT tutar, odendi FROM kasa WHERE aciklama LIKE '%$id%';";
-        $result = $conn->query($sql);
+    foreach ($reznoarray as $rezno) {
+        $sql = "SELECT tutar, odendi FROM kasa WHERE aciklama LIKE ? AND islem_tipi = 'G'";
+
+        $stmt = $conn->prepare($sql);
+
+        $param = '%' . $rezno . '%';
+        $stmt->bind_param("s", $param);
+
+        $stmt->execute();
+
+        $result = $stmt->get_result();
 
         if ($result->num_rows > 0) {
             while ($row = $result->fetch_assoc()) {
-                @$havaleinfo[$id][0] +=  floatval($row['tutar']);
-                @$havaleinfo[$id][1] .= $row['odendi'];
+                @$havaleinfo[$rezno]['tutar'] += floatval($row['tutar']);
+                @$havaleinfo[$rezno]['odeme'] .= $row['odendi'];
             }
-        }else{
-            $havaleinfo[$id][0] = 0;
-            $havaleinfo[$id][1] = "b";
+        } else {
+            $havaleinfo[$rezno][0] = 0;
+            $havaleinfo[$rezno][1] = "b";
         }
 
+        // Close the statement
+        $stmt->close();
     }
 
     return $havaleinfo;
 }
+
 function mergedata($ccdata,$havaledata,$idarray,$odemedata)
 {
     $finaldata = array();
 
     foreach ($idarray as $id) {
-        $finaldata[$id] = array(0, "", 0, 0);
+        $finaldata[$id] = array(array());
 
-        $finaldata[$id][3] = $id;
-        @$finaldata[$id][0] = $ccdata[$id][0];
-        @$finaldata[$id][1] = $ccdata[$id][1];
+        $finaldata[$id]['rezno'] = $id;
+        @$finaldata[$id]['tutar'] = $ccdata[$id]['tutar'];
+        @$finaldata[$id]['durum'] = $ccdata[$id]['odeme'];
 
-        $finaldata[$id][0] += $havaledata[$id][0];
-        $finaldata[$id][1] .= $havaledata[$id][1];
+        @$finaldata[$id]['tutar'] += $havaledata[$id]['tutar'];
+        @$finaldata[$id]['durum'] .= $havaledata[$id]['odeme'];
 
-        $finaldata[$id][2] = $odemedata[$id][1];
+        $finaldata[$id]['odenen'] = $odemedata[$id]['tutar'];
     }
     return $finaldata;
 }
 
 function odemedegerlendir($odenen,$tahsilat)
 {
-    global $ustlımıt;
-    global $altlımıt;
-
     return $tahsilat<=(USTLIMIT*$odenen) && $tahsilat>=(ALTLIMIT*$odenen);
 }
 function printres($id,$odenen,$tahsilat,$odendi,$err)
@@ -140,18 +154,18 @@ function process($data)
 {
     foreach ($data as $odeme){
         $errmsg = "";
-        if (!odemedegerlendir($odeme[2],$odeme[0])){
+        if (!odemedegerlendir($odeme['odenen'],$odeme['tutar'])){
             $errmsg .= "Tahsilat sorunu";
         }
-        if (odemekontrol($odeme[1])){
+        if (odemekontrol($odeme['durum'])){
             $errmsg .= "Zaten ödeme yapılmış";
         }
         if ($errmsg==""){
             $errmsg="Sorun yok";
-            printres($odeme[3],$odeme[2],$odeme[0],$odeme[1],$errmsg);
+            printres($odeme['rezno'],$odeme['tutar'],$odeme['odenen'],$odeme['durum'],$errmsg);
 
         }else{
-            printres($odeme[3],$odeme[2],$odeme[0],$odeme[1],$errmsg);
+            printres($odeme['rezno'],$odeme['tutar'],$odeme['odenen'],$odeme['durum'],$errmsg);
 
         }
     }
@@ -162,9 +176,9 @@ $odenenler_data=odenenler($mysqli);
 
 $idstring=preparestring($odenenler_data);
 
-$ccdata=getcc($idstring[0],$mysqli);
-$havaledata=gethavale($idstring[1],$mysqli);
+$ccdata=getcc($idstring,$mysqli);
+$havaledata=gethavale($idstring,$mysqli);
 $mysqli->close();
-$data = mergedata($ccdata,$havaledata,$idstring[1],$odenenler_data);
+$data = mergedata($ccdata,$havaledata,$idstring,$odenenler_data);
 
 process($data);
